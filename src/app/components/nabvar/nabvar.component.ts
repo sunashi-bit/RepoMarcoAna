439fcfbb42ad1a76e07f9e615129866bff55f06d
ItsAnita <aant25220@gmail.com>
2024-05-22 09:06:11 -0600

    navbar

---
+++ src/app/components/nabvar/nabvar.component.ts
@@ -0,0 +1,14 @@
+import { CommonModule } from '@angular/common';
+import { ChangeDetectionStrategy, Component } from '@angular/core';
+
+@Component({
+  selector: 'app-nabvar',
+  standalone: true,
+  imports: [
+    CommonModule,
+  ],
+  styleUrl: './nabvar.component.css',
+  templateUrl: './nabvar.component.html',
+  changeDetection: ChangeDetectionStrategy.OnPush,
+})
+export class NabvarComponent { }
